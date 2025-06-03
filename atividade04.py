261c57c8df2292fe3b50ec2e59bca2afea972ccf
mailonvitordeoliveiraciril <mailon.ciril@escola.pr.gov.br>
2025-06-03 12:15:12 +0000

    exercicio 05 terminado

---
+++ atividade04.py
@@ -0,0 +1,12 @@
+distancia=float(input("digite a distancia "))
+tempo=float(input("digite o tempo "))
+
+media= diatancia / temp
+   if v_media < 5:
+print("lento")
+
+elif v_media >=5 and <=10
+print("moderado")
+
+else:
+    print("rapido")
